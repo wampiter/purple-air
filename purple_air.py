dc75ea22a6538d77f6d88bae7713a2d81782a670
wampiter <wampiter@gmail.com>
2020-08-30 14:35:05 -0700

    enable multiple users

--- purple_air.py
+++ purple_air.py
@@ -4,6 +4,7 @@ from urllib.request import Request, urlopen
 import json
 from enum import Enum
 import os
+from time import sleep
 
 import config
 
@@ -11,14 +12,9 @@ PURPLE_AIR_URL_PREFIX = 'https://www.purpleair.com/json?show='
 smtp_server = "smtp.gmail.com"
 port = 587  # For starttls
 
-SENSOR_ID = config.sensor_id
+USER_CONFS = config.user_confs
 FILENAME = os.path.dirname(os.path.abspath(__file__)) + '/' + config.filename
-MIN_COLOR_NOTIF_THRESHOLD = config.min_color_notif_threshold
-MAX_COLOR_NOTIF_THRESHOLD = config.max_color_notif_threshold
-COUNTER_STRATEGY = config.counter # 'a' -> Counter 0, 'b' -> Counter 1, 'both' -> average of both
-CONVERSION_METHOD = config.conversion_method
 SENDER_EMAIL = config.sender_email
-TO_EMAILS = config.to_emails
 PASSWORD = config.password
 
 context = ssl.create_default_context()
@@ -65,42 +61,29 @@ def current_color(pm_2_5_value):
         return Color.hella_maroon #shit
 
 
-def get_sensor_data():
-    print("Attempting to open "+ PURPLE_AIR_URL_PREFIX + str(SENSOR_ID))
-    return json.load(urlopen(PURPLE_AIR_URL_PREFIX + str(SENSOR_ID)))
+def get_sensor_data(user_conf):
+    print("Attempting to open "+ PURPLE_AIR_URL_PREFIX + str(user_conf['sensor_id']))
+    return json.load(urlopen(PURPLE_AIR_URL_PREFIX + str(user_conf['sensor_id'])))
 
 
-def pm_2_5_average(data):
+def pm_2_5_average(user_conf, data):
     stats0 = json.loads(data["results"][0]["Stats"])
     stats1 = json.loads(data["results"][1]["Stats"])
 
-    if COUNTER_STRATEGY == "a":
+    if user_conf['counter'] == "a":
         reading = rstats0["v"]
-    elif COUNTER_STRATEGY == "b":
+    elif user_conf['counter'] == "b":
         reading = stats1["v"]
     else:
         reading = (stats0["v"] + stats1["v"])/2.
 
     # we only consider the lrapa conversion for now, though there are others.
-    if CONVERSION_METHOD == 'lrapa':
+    if user_conf['conversion_method'] == 'lrapa':
         return reading * 0.5 - 0.66 # see https://www.lrapa.org/DocumentCenter/View/4147/PurpleAir-Correction-Summary
     else:
         return reading
 
-def get_last_color():
-    try:
-        with open(FILENAME, 'r') as f:
-            color_name = f.read().strip()
-    except FileNotFoundError:
-        print('No previous color. Pretending it was green.')
-        color_name = 'green'
-    return Color[color_name]
-
-def update_color(color):
-    with open(FILENAME, 'w') as f:
-        f.write(color.name)
-
-def notify_color_change(old_color, new_color):
+def notify_color_change(user_conf, old_color, new_color):
     if old_color.value < new_color.value:
         print("degraded")
         message = "The air has degraded from " + old_color.name + " to " + new_color.name
@@ -108,49 +91,68 @@ def notify_color_change(old_color, new_color):
         print("improved")
         message = "The air has improved from " + old_color.name + " to " + new_color.name
     print("About to send notification of change")
-    send_gmail(SENDER_EMAIL, TO_EMAILS, PASSWORD, message)
+    send_gmail(SENDER_EMAIL, user_conf['to_emails'], PASSWORD, message)
 
 
-def should_notify_color_change(old_color, new_color):
-    if MIN_COLOR_NOTIF_THRESHOLD is not None:
-        big_enough = max(old_color.value, new_color.value) >= MIN_COLOR_NOTIF_THRESHOLD
+def should_notify_color_change(user_conf, old_color, new_color):
+    if user_conf['min_color_notif_threshold'] is not None:
+        big_enough = max(old_color.value, new_color.value) >= user_conf['min_color_notif_threshold']
     else:
         big_enough = True
-    if MAX_COLOR_NOTIF_THRESHOLD is not None:
-        small_enough = min(old_color.value, new_color.value) <= MAX_COLOR_NOTIF_THRESHOLD
+    if user_conf['max_color_notif_threshold'] is not None:
+        small_enough = min(old_color.value, new_color.value) <= user_conf['max_color_notif_threshold']
     else:
         small_enough = True
     return (big_enough and small_enough)
 
-def main():
-    print('Checking on sensor {} at {}...'.format(SENSOR_ID, datetime.now()))
+def do_user(user_conf):
+    '''Do everythign for a user'''
+    print('Checking on sensor {} at {}...'.format(user_conf['sensor_id'], datetime.now()))
     try:
-        current_data = get_sensor_data()
-        current_pm_2_5 = pm_2_5_average(current_data)
-
-        print("current pm2.5 reading is: " +  str(current_pm_2_5))
-
+        current_data = get_sensor_data(user_conf)
+        current_pm_2_5 = pm_2_5_average(user_conf, current_data)
         new_color = current_color(current_pm_2_5)
-        last_color = get_last_color()
-
-        if not new_color == last_color:
+        last_color = user_conf.get('last_color')
+        if (last_color is not None) and (new_color != Color[last_color]) :
             print("new color!")
-            update_color(new_color)
-            if should_notify_color_change(last_color, new_color):
+            last_color = Color[last_color]
+            if should_notify_color_change(user_conf, last_color, new_color):
                 print("sending color change notification")
-                notify_color_change(last_color, new_color)
+                notify_color_change(user_conf, last_color, new_color)
             else:
                 print("change outside notification thresholds")
         else:
             print("no change in color (still " + new_color.name + ")")
-    except:
-        print('Check failed!')
-        raise
+    except Exception as e:
+        print('Check failed for ' + str(user_conf['user']))
+        # send_gmail(SENDER_EMAIL, user_conf['to_emails'], PASSWORD, str(e))
+        print(e)
+        return user_conf['last_color']
     else:
         print('Check succeeded!')
-        return datetime.now()
+        return new_color.name
     finally:
         print('Check complete at {}'.format(str(datetime.now())))
+
+def main():
+    #Load file of prev colors
+    try:
+        with open(FILENAME, 'r') as f:
+            last_colors = json.loads(f.read())
+    except FileNotFoundError:
+        last_colors = {} #empty dict equivalent to no users
+    for user, color in last_colors.items():
+        USER_CONFS[user]['last_color'] = color
+
+    new_colors = {} #dict to store colors for each user
+    for user, user_conf in USER_CONFS.items():
+        user_conf['user'] = user
+        new_color = do_user(user_conf)
+        new_colors[user] = new_color
+        sleep(1) #avoid overloading URL
+
+    with open(FILENAME, 'w') as f:
+        f.write(json.dumps(new_colors))
 
 if __name__ == '__main__':
 	main()
--- sample_config.py
+++ sample_config.py
@@ -1,18 +1,21 @@
-#IMPORTANT: CHANGE THESE PARAMETERS
-#some number, mine is 5-digits long
-#find at https://www.purpleair.com/json
-sensor_id = 10842
 sender_email = 'from@gmail.com'
-to_emails = ['to@gmail.com']
-password = #gmail password, make sure unsecure 3rd party enabled
+password = 'my_password' #gmail password, make sure unsecure 3rd party enabled
+filename = 'air_colors.json' #you can leave this as is
 
-#These don't necessarily need to be changed
-#an int, which sets the minimum 'severity' for which to notify. e.g. `3` is orange
-min_color_notif_threshold = 3
-#sets max severity for which to nofity.
-max_color_notif_threshold = None
-#which counter you'd like to use readings from.
-#If not set to `a` or `b`, uses the average of both.
-counter = None
-conversion_method = None #only supported is 'lrapa'
-filename = 'air_color.txt'
+user_confs = {}
+
+user_confs['user1'] = {
+	'to_emails': ['to@gmail.com'],
+	#some number, mine is 5-digits long
+	#find at https://www.purpleair.com/json
+	'sensor_id': 10842,
+	#These don't necessarily need to be changed
+	#an int, which sets the minimum 'severity' for which to notify. e.g. `3` is orange
+	'min_color_notif_threshold': 3,
+	#sets max severity for which to nofity.
+	'max_color_notif_threshold': None,
+	#which counter you'd like to use readings from.
+	#If not set to `a` or `b`, uses the average of both.
+	'counter': None,
+	'conversion_method': None, #only supported is 'lrapa'
+	}
